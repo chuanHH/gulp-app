4f0a2290a61cb11c92dabb8548be77b42fc570e3
silence-j <974529279@qq.com>
2018-08-17 10:35:05 +0800

    mod

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,14 @@ const testEs6Js = (arg) =>{
     console.log(arg)  
   }
 testEs6Js(abc)
+$(function(){
+    $('.read-more').on('click',function () {
+        if ($('#readAll').html() === '阅读全文') {
+            $('.read-body').removeClass('maxStyle')
+            $('#readAll').html('收起')
+        } else {
+            $('.read-body').addClass('maxStyle')
+            $('#readAll').html('阅读全文')
+        }
+    })
+});
